f1827b6d35cb2f7fb9ef9d7eca384134c2106346
aert <dev.aert@gmail.com>
2014-01-05 20:29:30 +0100

    Correct RTD conf.

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 
-from ..version import get_git_version
+from version import get_git_version
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
